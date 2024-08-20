b41e740230e1957dc3c360379b52e0821412bc3e
Eric Seidel <github@eseidel.com>
2024-08-20 03:48:21 -0700

    test: fix spider test

--- test/creature_effects_test.dart
+++ test/creature_effects_test.dart
@@ -29,14 +29,14 @@ void main() {
     final enemy = creatureCatalog['Spider Level 1'];
     final result = doBattle(first: player, second: enemy);
     // Spider is faster than us so should do 3 damage on battle start.
-    // And then hit 3 more times (since it goes first).
-    expect(result.first.hp, 4);
+    // And then hit 2 more times (since it goes first).
+    expect(result.first.hp, 5);
     expect(result.winner, result.first);
 
     final player2 = createPlayer(intrinsic: const Stats(speed: 3));
     final result2 = doBattle(first: player2, second: enemy);
     // We are the same speed as the spider so it doesn't get the bonus damage
-    // and only hits twice (we kill it before it can hit a third time).
-    expect(result2.first.hp, 8);
+    // and only hits once (we kill it before it can hit a second time).
+    expect(result2.first.hp, 9);
   });
 }
