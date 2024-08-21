ae735227b23555922cfb573e26f74209b9527c3b
Eric Seidel <eric@shorebird.dev>
2024-08-20 23:33:59 -0700

    chore: add fancier example to default script

--- lib/he_is_coming.dart
+++ lib/he_is_coming.dart
@@ -7,9 +7,30 @@ import 'package:he_is_coming/src/logger.dart';
 void runSim() {
   data = Data.load();
 
-  final player = createPlayer(items: [data.items['Stone Steak']]);
-  final enemy = data.creatures['Spider Level 1'];
+  final items = [
+    'Granite Hammer',
+    'Elderwood Necklace',
+    'Iron Transfusion',
+    'Iron Transfusion',
+    'Iron Transfusion',
+    'Plated Helmet',
+    'Iron Transfusion',
+  ].map((name) => data.items[name]).toList();
+  final edge = data.edges['Bleeding Edge'];
+  final oils = [
+    data.oils['Attack Oil'],
+    data.oils['Armor Oil'],
+    data.oils['Speed Oil'],
+  ];
+  final player = createPlayer(items: items, edge: edge, oils: oils);
+  final enemy = data.creatures['Woodland Abomination'];
 
-  final result = Battle.resolve(first: player, second: enemy);
-  logger.info('${result.winner.name} wins!');
+  // final player = createPlayer(items: [data.items['Stone Steak']]);
+  // final enemy = data.creatures['Spider Level 1'];
+
+  final result = Battle.resolve(first: player, second: enemy, verbose: true);
+  final winner = result.winner;
+  final damageTaken = winner.baseStats.maxHp - winner.hp;
+  logger.info('${result.winner.name} wins in ${result.turns} turns with '
+      '$damageTaken damage taken.');
 }
