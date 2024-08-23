8f1f201f8a1fbcfc2edc93c20b5446190be9cfd5
Eric Seidel <eric@shorebird.dev>
2024-08-23 14:03:21 -0700

    feat: add basic items list

--- ui/lib/main.dart
+++ ui/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:he_is_coming/src/data.dart';
+import 'package:he_is_coming/src/item.dart' as i;
 
 void main() {
   runApp(const MyApp());
@@ -34,6 +35,81 @@ class MyHomePage extends StatefulWidget {
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
+}
+
+extension on i.Item {
+  Color get color {
+    if (kind == i.Kind.weapon) {
+      return Colors.red;
+    }
+    if (material == i.Material.stone) {
+      return Colors.grey;
+    }
+    if (material == i.Material.sanguine) {
+      return Colors.red;
+    }
+    return Colors.orange;
+  }
+
+  IconData get icon {
+    return Icons.help;
+  }
+
+  String get description {
+    final parts = <String>[];
+    if (effect != null) {
+      parts.add(effect!.text);
+    }
+    if (stats.attack > 0) {
+      parts.add('⚔️ ${stats.attack}');
+    }
+    if (stats.maxHp > 0) {
+      parts.add('❤️ ${stats.maxHp}');
+    }
+    if (stats.armor > 0) {
+      parts.add('🛡️ ${stats.armor}');
+    }
+    if (stats.speed > 0) {
+      parts.add('👟 ${stats.speed}');
+    }
+    // Tags
+    if (isUnique) {
+      parts.add('unique');
+    }
+    if (kind == i.Kind.food) {
+      parts.add('food');
+    }
+    if (kind == i.Kind.jewelry) {
+      parts.add('jewelry');
+    }
+    if (material == i.Material.stone) {
+      parts.add('stone');
+    }
+    if (material == i.Material.sanguine) {
+      parts.add('sanguine');
+    }
+    if (material == i.Material.wood) {
+      parts.add('wood');
+    }
+    return parts.join(' ');
+  }
+}
+
+class ItemView extends StatelessWidget {
+  const ItemView({
+    required this.item,
+    super.key,
+  });
+  final i.Item item;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Icon(item.icon, color: item.color),
+      title: Text(item.name),
+      subtitle: Text(item.description),
+    );
+  }
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -79,7 +155,12 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: isLoading
             ? const CircularProgressIndicator()
-            : Text(data.creatures.creatures.first.name),
+            : ListView.builder(
+                itemCount: data.items.items.length,
+                itemBuilder: (context, index) {
+                  return ItemView(item: data.items.items[index]);
+                },
+              ),
       ),
     );
   }
